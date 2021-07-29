77022cf6fbb6c03f4095f0c2e5ecf203215eb5c7
Ambuj Bharati <Ambujgoswami658@gmail.com>
2021-07-29 14:09:21 +0530

    version 2
    responsive css addition and high score module addition

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,4 @@
+document.getElementById("bt").style.display = "none";
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -6,80 +7,139 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keypress(function(){
-  if(!started){
-    $("#level-title").text("Level "+level);
-    nextSequence();
-    started = true;
+// var ready;
+// var name = prompt("Please enter your name :", "Harry Potter");
+// if (name == null || name == "") {
+//   ready = 0;
+// } else {
+//   ready = 1;
+// }
+
+const NO_OF_HIGH_SCORES = 3;
+const HIGH_SCORES = 'highScores';
+
+showHighScores();
+//if (ready == 1) {
+  $(document).keypress(start);
+
+  $(".btn").click(start);
+
+  function start() {
+    document.getElementById("bt").style.display = "none";
+    if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
   }
-});
 
-$(".btn").click(function(){
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
+  $(".btnl").click(function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
 
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 
-  checkAnswer(userClickedPattern.length-1);
-});
+    checkAnswer(userClickedPattern.length - 1);
+  });
 
-function checkAnswer(currentLevel)
-{
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
+  function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length) {
+        setTimeout(function() {
           nextSequence();
         }, 1000);
       }
     } else {
       playSound("wrong");
       $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-
-      setTimeout(function () {
+      $("#level-title").text("Game Over, Press Any Key to Restart OR");
+      document.getElementById("bt").style.display = "block";
+      setTimeout(function() {
         $("body").removeClass("game-over");
       }, 200);
 
+      checkHighScore(level);
       startOver();
     }
-}
+  }
 
-function nextSequence()
-{
-  userClickedPattern = [];
-  level++;
-  $("#level-title").text("Level " + level);
+  function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
 
-  var randomNumber = Math.random();
-  randomNumber = Math.floor(randomNumber*4);
+    var randomNumber = Math.random();
+    randomNumber = Math.floor(randomNumber * 4);
 
-  var randomChosenColour = buttonColours[randomNumber];
+    var randomChosenColour = buttonColours[randomNumber];
 
-  gamePattern.push(randomChosenColour);
+    gamePattern.push(randomChosenColour);
 
-  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  playSound(randomChosenColour);
-}
+    playSound(randomChosenColour);
+  }
 
-function playSound(name)
-{
-  var audio = new Audio("sounds/"+name+".mp3");
-  audio.play();
-}
+  function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+  }
 
-function animatePress(currentColor)
-{
-  $("#"+currentColor).addClass("pressed");
-  setTimeout(function () {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
-}
+  function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function() {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+  }
 
-function startOver()
-{
-  level = 0;
-  gamePattern = [];
-  started = false;
-}
+  function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+  }
+
+  function checkHighScore(score) {
+    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
+
+    if (score > lowestScore) {
+      saveHighScore(score, highScores); // TODO
+      showHighScores(); // TODO
+    }else{
+      alert("Sorry your score is lower than top 3, try again");
+    }
+  }
+
+  function saveHighScore(score, highScores) {
+    const name = prompt("You got a highscore! Enter name :","Harry potter");
+    const newScore = {
+      score,
+      name
+    };
+
+    // 1. Add to list
+    highScores.push(newScore);
+
+    // 2. Sort the list
+    highScores.sort((a, b) => b.score - a.score);
+
+    // 3. Select new list
+    highScores.splice(NO_OF_HIGH_SCORES);
+
+    // 4. Save to local storage
+    localStorage.setItem(HIGH_SCORES, JSON.stringify(highScores));
+  }
+
+  function showHighScores() {
+    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    const highScoreList = document.getElementById(HIGH_SCORES);
+
+    highScoreList.innerHTML = highScores
+      .map((score) => `<li>${score.name} : ${score.score}`)
+      .join('');
+  }
+
+// } else {
+//   location.replace("index.html");
+// }
